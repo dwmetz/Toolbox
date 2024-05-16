4437d482d64bd91ae593e0077567bbab421155b8
Doug Metz <dwmetz@gmail.com>
2024-05-16 09:57:29 -0400

    Update Strings2Yara.py
    "all" and MZ header

--- Strings2Yara.py
+++ Strings2Yara.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-# # Takes the input of strings.txt, prompts for author and rule metadata, and create output in propperly formatted YARA rule
-# # Defaults to 'all of'.  Also watch your special characters.  It's not perfect but it can get the process started.
+#  Takes the input of strings.txt, prompts for author and rule metadata, and create output in propperly formatted YARA rule
+#  Defaults to 'all of'.  Also watch your special characters.  It's not perfect but it can get the process started.
 
-# # Sets up the YARA rule metadata. You can hardcode author.
+#  Sets up the YARA rule metadata and formatting. You can hardcode author.
 def get_user_input():
     rule_name = input("Enter the rule name: ")
     author = input("Enter the author: ")
     description = input("Enter the description: ")
     hash_value = input("Enter the hash value: ")
     return rule_name, author, description, hash_value
-
-# Create the YARA rule with tabs and formatting
+#  Create the YARA rule tabs and formatting
 def create_yara_rule(rule_name, author, description, hash_value, strings_file):
     yara_rule = f'''rule {rule_name} {{
     meta:
@@ -26,19 +25,19 @@ def create_yara_rule(rule_name, author, description, hash_value, strings_file):
             yara_rule += f'\t$s{id} = "{line.strip()}"\n\t'
     yara_rule += '\n'
     yara_rule += '\tcondition:\n'
-    yara_rule += '\t\tany of them\n}\n'
+    yara_rule += '\t\tuint16be(0) == 0x4d5a and\n'
+    yara_rule += '\t\tall of them\n}\n'
 
     return yara_rule
-
+# Assembly of the YARA rule
 def main():
     rule_name, author, description, hash_value = get_user_input()
-    strings_file = 'strings.txt'  # Path to your strings file
+    strings_file = 'strings.txt'  
 
     yara_rule = create_yara_rule(rule_name, author, description, hash_value, strings_file)
     print("Generated YARA rule:")
     print(yara_rule)
     
-# Save the YARA rule to a .yar file
     yar_filename = f'{rule_name}.yar'
     with open(yar_filename, 'w') as yar_file:
         yar_file.write(yara_rule)
@@ -47,6 +46,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
 
 
 
